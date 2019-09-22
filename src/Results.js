c09375b7f615987ae0a1c03e4980daa48ef30682
Dimitri Marion <dimitri.marion@gmail.com>
2019-09-22 17:24:08 +0200

    change name's species in ant card

--- src/Results.js
+++ src/Results.js
@@ -3,7 +3,7 @@ import antImg from "../assets/img/ant.svg";
 
 const Results = props => {
   const URL =
-    "https://api.gbif.org/v1/occurrence/search/?datasetKey=13b70480-bd69-11dd-b15f-b8a03c50a862&familyKey=4342&limit=100&country=";
+    "https://api.gbif.org/v1/occurrence/search/?datasetKey=13b70480-bd69-11dd-b15f-b8a03c50a862&familyKey=4342&limit=16&offset=0&country=";
   const [species, setSpecies] = useState([]);
 
   useEffect(() => {
@@ -26,20 +26,16 @@ const Results = props => {
   return (
     <main className="ant-page">
       <section className="ant-result">
-        {species.map(({ key, scientificName, country, media }) => (
+        {species.map(({ key, species, scientificName, country, media }) => (
           <div className="ant-card" key={key}>
-            <img
-              src={media ? filterMedia(media) : antImg}
-              alt={scientificName}
-            />
+            <img src={media ? filterMedia(media) : antImg} alt={species} />
             <div className="ant-description">
-              <p className="ant-name">{scientificName}</p>
+              <p className="ant-name">{species ? species : scientificName}</p>
               <p>{country}</p>
             </div>
           </div>
         ))}
       </section>
-      x
     </main>
   );
 };
