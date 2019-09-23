185f78186e488bf1321430695cce1236d4055e99
Dimitri Marion <dimitri.marion@gmail.com>
2019-09-23 15:37:25 +0200

    add pagination on result page

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import antImg from "../assets/img/ant.svg";
+import ReactPaginate from "react-paginate";
 import { navigate } from "@reach/router";
 import Ants from "./Ants";
 
@@ -11,19 +11,19 @@ const Results = props => {
   useEffect(() => {
     console.log("setOffset");
     setOffset((props.pageId - 1) * LIMIT);
-  }, [props.pageId, activepage]);
+  }, [props.pageId]);
 
   const URL = `https://api.gbif.org/v1/occurrence/search/?datasetKey=13b70480-bd69-11dd-b15f-b8a03c50a862&familyKey=4342&limit=${LIMIT}&offset=${offset}&country=`;
 
   const [species, setSpecies] = useState([]);
   const [speciCount, setSpeciCount] = useState(0);
   const [loading, setLoading] = useState(true);
-  const [pageCount, setPageCount] = useState(0);
 
   useEffect(() => {
     fetch(`${URL}${props.country}`)
       .then(response => response.json())
       .then(data => {
+        console.log("useEffect Species");
         console.log(data);
         const { results } = data;
         const { count } = data;
@@ -31,17 +31,19 @@ const Results = props => {
 
         setSpecies(results);
         setSpeciCount(count);
-        setPageCount(Math.ceil(count / LIMIT));
         setLoading(false);
       });
-  }, [URL, props.country, activepage]);
+    return () => {
+      console.log("cleanup");
+      setLoading(true);
+    };
+  }, [URL, props.country]);
 
-  const filterMedia = media => {
-    const mediaHead = media.filter(item => item.identifier.includes("h_1"));
-    return mediaHead.length != 0
-      ? mediaHead[0].identifier.replace("high", "thumbview")
-      : antImg;
-  };
+  const [pageCount, setPageCount] = useState(0);
+  useEffect(() => {
+    console.log("useEffect pageCount");
+    setPageCount(Math.ceil(speciCount / LIMIT));
+  }, [setSpeciCount, speciCount]);
 
   if (loading) {
     return <h1>Loading ...</h1>;
@@ -53,29 +55,24 @@ const Results = props => {
             {speciCount} specimens in {props.location.state.country.name}.
           </h1>
           <Ants species={species} />
-          <nav>
-            {speciCount < LIMIT ? (
-              ""
-            ) : (
-              <ul className="pagination">
-                <li>
-                  <button>{offset / LIMIT + 1}</button>
-                </li>
-                <li>
-                  <button
-                    onClick={() => {
-                      setActivepage(2);
-                      navigate(`/results/${props.country}/page/2`);
-                    }}
-                  >
-                    {offset / LIMIT + 2}
-                  </button>
-                </li>
-                <li>
-                  <button>{offset / LIMIT + 3}</button>
-                </li>
-              </ul>
-            )}
+          <nav className="nav-pagination">
+            <ReactPaginate
+              pageCount={pageCount}
+              pageRangeDisplayed={3}
+              marginPagesDisplayed={1}
+              containerClassName="pagination"
+              previousLabel="&laquo;"
+              nextLabel="&raquo;"
+              activeClassName="active-page"
+              onPageChange={({ selected }) => {
+                console.log("selected " + selected);
+                setActivepage(selected + 1);
+                navigate(`/results/${props.country}/page/${selected + 1}`, {
+                  state: { country: props.location.state.country }
+                });
+              }}
+              forcePage={activepage - 1}
+            />
           </nav>
         </main>
         <footer className="result-footer">
