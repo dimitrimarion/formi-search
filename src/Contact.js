4edb2a7a7efc7f13c73158f0607bca1b0b36aca5
Dimitri Marion <dimitri.marion@gmail.com>
2019-09-24 16:19:44 +0200

    implement form submission in contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,23 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  };
+
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
   return (
     <main>
       <h1 className="title">Contact</h1>
-      <form name="contact" method="post" className="contactForm">
+      <form
+        name="contact"
+        method="post"
+        className="contactForm"
+        onSubmit={e => {
+          e.preventDefault();
+          handleSubmit(e);
+        }}
+      >
         <p>
           <label htmlFor="name">Your Name</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
         </p>
 
         <p>
           <label htmlFor="email">Your Email</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
         </p>
 
         <p>
           <label htmlFor="message">Message</label>
-          <textarea name="message"></textarea>
+          <textarea
+            name="message"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          ></textarea>
         </p>
 
         <button type="submit">Send</button>
