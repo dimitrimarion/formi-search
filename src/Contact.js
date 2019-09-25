92a5d28e8446cfa25178801b13ed7d99ce2e3006
Dimitri Marion <dimitri.marion@gmail.com>
2019-09-25 09:50:47 +0200

    modify state in handleSubmit

--- src/Contact.js
+++ src/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+      body: encode({ "form-name": "contact", name, email, message })
     })
       .then(() => alert("Success!"))
       .catch(error => alert(error));
